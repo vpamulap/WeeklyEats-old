eb89da6ecfe01c8db637c912f83202f273188059
Vivek Pamulapati <vpamulap@gmail.com>
2011-11-20 15:00:20 -0500

    added persistence

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,18 +4,26 @@ class DashboardController < ApplicationController
   
   def main
     # Get correct number of recipes
-    nmeals = params[:mealnumber].to_i
-    @recipes = Recipe.all.sample(nmeals)
+    if (session[:rids] == nil) || (session[:rids].length == 0)
+      nmeals = params[:mealnumber].to_i
+      @recipes = Recipe.all.sample(nmeals)
     
     # Get recipe id's and set initial confirmations to false
-    recipe_ids = []
-    recipe_conf = []
-    @recipes.each do |recipe|
+      recipe_ids = []
+      recipe_conf = []
+      @recipes.each do |recipe|
         recipe_ids << recipe.id
         recipe_conf << false
+      end
+      
+      session[:rids] = recipe_ids
+      session[:rc] = recipe_conf
+    else
+      @recipes = Recipe.all
+      @recipes.delete_if do |recipe|
+        not session[:rids].include?(recipe.id)
+      end
     end
-    session[:rid] = recipe_ids
-    session[:rc] = recipe_conf
   end
 
   def stats
