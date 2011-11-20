11ad7dd2ade3a68976c67d67c1afc9865301105e
Vivek Pamulapati <vpamulap@gmail.com>
2011-11-20 12:40:45 -0500

    added session info

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,19 +7,23 @@ class DashboardController < ApplicationController
     nmeals = params[:mealnumber].to_i
     @recipes = Recipe.all.sample(nmeals)
     
-    # Get recipe id's
+    # Get recipe id's and set initial confirmations to false
     recipe_ids = []
+    recipe_conf = []
     @recipes.each do |recipe|
         recipe_ids << recipe.id
+        recipe_conf << false
     end
-    
-    
+    session[:rid] = recipe_ids
+    session[:rc] = recipe_conf
   end
 
   def stats
   end
 
   def recipe
+    recipe_id = params[:recipe_id].to_i
+    @recipe = Recipe.all[recipe_id]
   end
   
   def setup
@@ -28,3 +32,6 @@ class DashboardController < ApplicationController
   def shopping
   end
 end
+
+
+
